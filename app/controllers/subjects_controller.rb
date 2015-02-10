403b7faf359e04aa53659e48a61b1abc217bd0ba
NEHA SUWAl <neha@jyaasa.com>
2015-02-10 12:05:40 +0545

    added 'destroy' action to delete the subject

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -25,13 +25,29 @@ class SubjectsController < ApplicationController
   end
 
   def edit
+    @subject = Subject.find(params[:id])
+  end
+
+  def update
+    @subject = Subject.find(params[:id])
+    if @subject.update_attributes(subject_params)
+      redirect_to(:action => 'show', :id => @subject.id)
+    else
+      render('edit')
+    end
   end
 
   def delete
+    @subject = Subject.find(params[:id])
+  end
+
+  def destroy
+    subject = Subject.find(params[:id]).destroy
+    redirect_to(:action => 'index')
   end
 
   private
-  
+
     def subject_params
       params.require(:subject).permit(:name, :position, :visible)
     end
