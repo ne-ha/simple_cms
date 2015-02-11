d3c032006bcb5e1692c27dbafeec7f1255839f55
NEHA SUWAl <neha@jyaasa.com>
2015-02-11 07:07:06 +0545

    managed routes for admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root "demo#index"
+
+  get 'admin',  :to => "access#index"
   # get 'demo/index'
   match ':controller(/:action(/:id))', :via => [:get, :post] 
 
